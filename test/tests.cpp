ebb3847f80b1c3a052a462bb2faf2a410ed74f67
samanseifi <samansei@bu.edu>
2024-02-09 20:02:09 -0500

    #1 establishing the GoogleTest framework

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,49 +1,36 @@
-//
-// Created by saman on 10/29/20.
-//
+// Assuming you have GoogleTest included in your project setup
 
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_MODULE my_unit_tests
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
-
-
+#include <gtest/gtest.h>
 #include "../includes/Vec3.h"
 #include "../includes/ArrayT.h"
 
+// Define the test suite name with TEST()
+TEST(MyTestSuite, DotProduct) {
+    Vec3 v1(1.0, 2.0, 0.0);
+    Vec3 v2(-1.0, -3.0, 1.0);
+    EXPECT_DOUBLE_EQ(v1.Dot(v2), -7.0); // Use EXPECT_DOUBLE_EQ for comparing floating-point numbers
+}
 
-BOOST_AUTO_TEST_SUITE(my_testsuite)
+TEST(MyTestSuite, AssignToScalar) {
+    Vec3 v1(3, 5, 0);
+    double val = 3;
+    v1 = val;
+    EXPECT_EQ(v1.x, 3);
+    EXPECT_EQ(v1.y, 3);
+}
 
-    BOOST_AUTO_TEST_CASE(dot_product)
-    {
-        Vec3 v1(1.0, 2.0, 0.0);
-        Vec3 v2(-1.0, -3.0, 1.0);
-        BOOST_TEST ( v1.Dot(v2) == -7.0);
-    }
+TEST(MyTestSuite, MultiplyingByScalar) {
+    Vec3 v1(5, 4, 0);
+    double val = 3;
+    v1 *= val;
+    EXPECT_EQ(v1.x, 15);
+    EXPECT_EQ(v1.y, 12);
+}
 
-    BOOST_AUTO_TEST_CASE(assign_to_scalar)
-    {
-        Vec3 v1(3, 5, 0);
-        double val = 3;
-        v1 = val;
-        BOOST_TEST (v1.x == 3);
-        BOOST_TEST (v1.y == 3);
-    }
-    BOOST_AUTO_TEST_CASE(multiplying_by_scalar)
-    {
-        Vec3 v1(5, 4, 0);
-        double val = 3;
-        v1 *= val;
-        BOOST_TEST (v1.x == 15);
-        BOOST_TEST (v1.y == 12);
-    }
-    BOOST_AUTO_TEST_CASE(multiplying_by_scalar_on_the_line)
-    {
-        Vec3 v1(2.0, 3.0, 0.0);
-        double val = 2.0;
-        Vec3 v2 = v1 * val;
-        BOOST_TEST (v2.x == 4.0);
-        BOOST_TEST (v2.y == 6.0);
-    }
-    
-BOOST_AUTO_TEST_SUITE_END()
+TEST(MyTestSuite, MultiplyingByScalarOnTheLine) {
+    Vec3 v1(2.0, 3.0, 0.0);
+    double val = 2.0;
+    Vec3 v2 = v1 * val;
+    EXPECT_EQ(v2.x, 4.0);
+    EXPECT_EQ(v2.y, 6.0);
+}
